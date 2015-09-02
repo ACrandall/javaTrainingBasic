6bbe7acd232f5c89a7969ef38e25cb42bd4b7b1c
Adam B. Crandall <AdamBCrandall@yahoo.com>
2015-09-02 14:34:54 -0500

    Inital Commit

---
+++ src/helloWorldApp.java
@@ -0,0 +1,12 @@
+/**
+ * Created by acrandall on 9/2/2015.
+ */
+public class helloWorldApp {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); //Display the String.
+
+    }
+}
