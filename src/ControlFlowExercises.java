097880f11a49e86d586089fd58adb3c91671df7c
Adam B. Crandall <AdamBCrandall@yahoo.com>
2015-12-08 13:41:09 -0600

    12/8 Training with Jana. Added ControlFlowExercises to code.

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,20 @@
+/**
+ * Created by acrandall on 12/7/2015.
+ *
+ * Showing off control flow structures
+ */
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+        int aNumber = 3;
+
+        if (aNumber >= 0) {
+            if (aNumber == 0) {
+                System.out.println("first string");
+            } else {
+                System.out.println("second string");
+            }
+        }
+
+        System.out.println("third string");
+    }
+}
