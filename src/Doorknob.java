a4eb034809b5e197c876b99fd25656754e07f1e1
j_schroeder <jana-schroeder@idexx.com>
2016-03-14 11:06:14 -0500

    Pi date 3-4-16 Added a copy constructor to doorknob

--- src/Doorknob.java
+++ src/Doorknob.java
@@ -1,17 +1,22 @@
 /**
  * Created by acrandall on 2/24/2016.
  */
-public class Doorknob {
+public class Doorknob {//class
     double radius;
     String material;
 
-    public Doorknob(){
+    public Doorknob(){// no arg constructor or default constructor
         radius = 2;
         material = "Glass";
     }
 
-    public Doorknob(double radius, String material){
+    public Doorknob(double radius, String material){//constructor or parameterized constructor
         this.radius = radius;
         this.material = material;
+    }
+
+    public Doorknob(Doorknob myDoorKnob){//copy constructor
+        this.radius = myDoorKnob.radius; //this Doorknobs radius is the same as myDoorknobs radius
+        this.material = myDoorKnob.material; //this Doorknobs material is the same as myDoorknobs material
     }
 }
